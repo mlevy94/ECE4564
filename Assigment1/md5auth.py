9c393bb0a2eb4264c22a48235c7bee8c7cb820e6
jacques <jacques@vt.edu>
2017-02-11 19:47:19 -0500

    now works for lists

--- Assigment1/md5auth.py
+++ Assigment1/md5auth.py
@@ -1,16 +1,24 @@
 import hashlib
 
 def createMD5(inString):
+    hashList = []
     m = hashlib.md5()
-    m.update(inString.encode())
-    return m.hexdigest()
+    if isinstance(inString, (tuple,list)):
+        for entry in inString:
+            m.update(entry.encode())
+            hashList.append(m.hexdigest())
+        return hashList
+    else:
+        m.update(inString.encode())
+        return m.hexdigest()
 
 def authMD5(inHash, inString):
+    hashList = []
     m = hashlib.md5()
-    m.update(inString.encode())
-
-    if (inHash == m.hexdigest()):
-        return True
-    return False
+    for entry, verHash in zip(inString, inHash):
+        m.update(entry.encode())
+        if m.hexdigest() != verHash:
+                return False
+    return True
 
 
