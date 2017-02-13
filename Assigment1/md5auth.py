741e1a83cd4919113d1871dc6df73723d40bcfa9
jacques <jacques@vt.edu>
2017-02-13 18:28:45 -0500

    added comments

--- Assigment1/md5auth.py
+++ Assigment1/md5auth.py
@@ -1,17 +1,27 @@
 import hashlib
 
+# createMD5
+# @breif	Creates a list of md5 hashes given a list of input strings
+# @param	inString	A list or tuple of input strings
+# @return	hashlist	A list of md5 hashes corresponding to input strings
 def createMD5(inString):
     hashList = []
     m = hashlib.md5()
     if isinstance(inString, (tuple,list)):
-        for entry in inString:
+        for entry in inString:		#create a list of md5 hashes
             m.update(entry.encode())
             hashList.append(m.hexdigest())
         return hashList
-    else:
-        m.update(inString.encode())
-        return m.hexdigest()
+    else:#no strings allowed, must be list of strings
+        raise Exception("Input must be a list or tuple")
 
+# authMD5 
+# @breif	Verifies a list of strings given a list of hashes. Returns true
+#			when all strings match the given hashes, or false otherwise
+# @param	inHash		A list of hashes to test
+# @param	inString	A list of strings to verify
+# @retval	True		Strings successfully authenticated
+# @retval	False		Authentication failed
 def authMD5(inHash, inString):
     hashList = []
     m = hashlib.md5()
