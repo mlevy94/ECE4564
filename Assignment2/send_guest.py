44ae87cc0e77e4f60b053c4bd12f57f101182ef5
jacques <jacques@vt.edu>
2017-02-25 17:51:59 -0500

    test guest

---
+++ Assignment2/send_guest.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import pika
+import sys
+
+
+credentials = pika.PlainCredentials('guest', 'guest')
+parameters = pika.ConnectionParameters('172.29.120.1',
+                                       5672,
+                                       'jacques',
+                                       credentials)
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+mess = sys.argv[1]
+
+while mess != "quit":
+    channel.basic_publish(exchange='',
+                          routing_key='hello',
+                          body=mess)
+    print(" [x] Sent", mess)
+    mess = input('Message to send: ')
+    
+connection.close()
