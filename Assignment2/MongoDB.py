79604fd7daf443f4ff41c90cb915031c98f115b5
Hunter Wallen <Huntw94@vt.edu>
2017-02-25 19:03:40 -0500

    added in more lines

--- Assignment2/MongoDB.py
+++ Assignment2/MongoDB.py
@@ -3,63 +3,44 @@ from pymongo import MongoClient
 
 import json
 
-def findHigh(num1, num2):
-    if (num1 >= num2):
-        High = num1
-    else:
-        High = num2
-    print(High)
 
 
-def findLow(num1, num2):
-    if (num1 <= num2):
-        Low = num1
-    else:
-        Low = num2
-    print(Low)
+
+
+
+
 
 client = MongoClient('localhost', 27017)
 
 db = client.P2_database
 
 pi1 = db.pi1_collection
+s = '{"Pi": "pi1","net": {"lo": {"rx": 0,"tx": 0},"wlan0":{"rx": 708,"tx": 1192},"eth0": {"rx": 0,"tx": 0}},"cpu": 0.8393824}'
+str = json.loads(s)
+identity = str["Pi"]
+lorx = str["net"]["lo"]["rx"]
+lotx = str["net"]["lo"]["tx"]
+wlanrx = str["net"]["wlan0"]["rx"]
+wlantx = str["net"]["wlan0"]["tx"]
+ethrx = str["net"]["eth0"]["rx"]
+ethtx = str["net"]["eth0"]["tx"]
+cputime = str["cpu"]
 
-lorx
-lotx
-wlanrx
-wlantx
-ethrx
-ethtx
-cputime
 
 
-pi1.delete_many({})
 
-
-info = ({"Pi": "pi1",
-         "net": {"lo": {"rx": lorx, "tx": lotx },
-         "wlan0": {"rx": wlanrx, "tx": wlantx},
-         "eth0": {"rx": ethrx, "tx": ethtx}},
+info = ({"Pi": identity,
+         "net": {
+              "lo": {"rx": lorx , "tx": lotx},
+              "wlan0": {"rx": wlanrx, "tx": wlantx},
+              "eth0": {"rx": ethrx, "tx": ethtx}},
          "cpu": cputime})
 
+#pi1_id = pi1.insert_one(info).inserted_id
 
+print(pi1.find_one({"Pi": "pi1"})["cpu"])
+print(pi1.find({"Pi": "pi1"}).count())
 
-pi1_id = pi1.insert_one(info).inserted_id
-
-
-
-
-
-findHigh(120, int(pi1.find_one({"author": "Hunter"})["cpu"]))
-print(pi1.find_one({"author": "Hunter"})["cpu"])
-print(pi1.find({"author":"Hunter"}).count())
-
-
-# placeholders
-# cpu
-
-# l0
-# eth0
-# wlan0
-
-
+for doc in pi1.find().sort('cpu', pymongo.DESCENDING):
+    print(doc)
+    break
