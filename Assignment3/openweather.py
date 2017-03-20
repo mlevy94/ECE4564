03e06da5a02433676683ba74f6c788d0a16ef427
Michael Levy <mlevy94@vt.edu>
2017-03-20 14:22:12 -0400

    Minor text fixes.

--- Assignment3/openweather.py
+++ Assignment3/openweather.py
@@ -4,11 +4,11 @@ import json
 
 
 fiveDay = "http://api.openweathermap.org/data/2.5/forecast?zip={zip},us&APPID={key}"
-sixteenDay = "http://api.openweathermap.org/data/2.5/forecast/daily?zip={zip},us&APPID={key}"
+sixteenDay = "http://api.openweathermap.org/data/2.5/forecast/daily?zip={zip},us&cnt=16&APPID={key}"
 
 zip = 24060
 
-apiKey = "327ac52ecbb2877d22adb0efdc283505"
+apiKey = "231ea1f95f5b7e73a482ffcdc9772060"
 
 def getWeather(zipcode, startTime):
   if isinstance(startTime, float):
@@ -16,7 +16,7 @@ def getWeather(zipcode, startTime):
   request = sixteenDay.format(zip=zipcode, key=apiKey)
   answer = requests.get(request)
   if not answer.ok:
-    raise ConnectionError("Bad API Response: code {}, request: ".format(answer.status_code, request))
+    raise ConnectionError("Bad API Response: code {}, request: {}".format(answer.status_code, request))
   forcastList = json.loads(answer.text)["list"]
   targetDay = None
   for day in forcastList:
@@ -24,7 +24,6 @@ def getWeather(zipcode, startTime):
     if forcastDate.date() == startTime.date():
       targetDay = day
       break
-  import pdb; pdb.set_trace()
   try:
     return targetDay["clouds"]
   except TypeError:
