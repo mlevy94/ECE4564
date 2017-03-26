b9e58dce784b146b64a9fd5c70033e7a79cd9d1d
jacques <jacques@vt.edu>
2017-03-26 12:33:53 -0400

    initial alert song commit

---
+++ Assignment3/alert_song.py
@@ -0,0 +1,12 @@
+import pygame
+
+
+def play(songname):
+    pygame.mixer.init()
+    pygame.mixer.music.load(songname)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        continue
+
+if __name__ == "__main__":
+    play("odyssey.wav")
