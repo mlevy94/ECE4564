2e8887483abd84f8b2cc175124ca35a47e5e88af
jacques <jacques@vt.edu>
2017-04-08 17:20:23 -0400

    fixed spacing

--- Assignment4/playerled.py
+++ Assignment4/playerled.py
@@ -2,31 +2,31 @@ import RPi.GPIO as GPIO
 import time
 
 def color(player):
-	GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     GPIO.setup(4,GPIO.OUT)
-	GPIO.setup(17,GPIO.OUT)
-	GPIO.setup(27,GPIO.OUT)
+    GPIO.setup(17,GPIO.OUT)
+    GPIO.setup(27,GPIO.OUT)
 	
-	col = player % 3;
+    col = player % 3;
 	
-	if col == 0:
-		GPIO.output(17,GPIO.LOW)
-		GPIO.output(27,GPIO.LOW)
-		GPIO.output(4,GPIO.HIGH)
+    if col == 0:
+	GPIO.output(17,GPIO.LOW)
+	GPIO.output(27,GPIO.LOW)
+	GPIO.output(4,GPIO.HIGH)
 	
     else if col == 1:
-		GPIO.output(4,GPIO.LOW)
-		GPIO.output(27,GPIO.LOW)
-		GPIO.output(17,GPIO.HIGH)
+	GPIO.output(4,GPIO.LOW)
+	GPIO.output(27,GPIO.LOW)
+	GPIO.output(17,GPIO.HIGH)
 		
-	else:
-		GPIO.output(4,GPIO.LOW)
-		GPIO.output(17,GPIO.LOW)
-		GPIO.output(27,GPIO.HIGH)
+    else:
+	GPIO.output(4,GPIO.LOW)
+	GPIO.output(17,GPIO.LOW)
+	GPIO.output(27,GPIO.HIGH)
 		
-	finally:
-		GPIO.cleanup()
+    finally:
+	GPIO.cleanup()
 
 if __name__ == "__main__":
     color(0)
