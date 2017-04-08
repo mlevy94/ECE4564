ef5a872f264d8706f2bf02ba262804b62e969973
jacques <jacques@vt.edu>
2017-04-08 17:38:33 -0400

    removed cleanup

--- Assignment4/playerled.py
+++ Assignment4/playerled.py
@@ -3,37 +3,40 @@ import time
 
 
 def color(player):
-    try:
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
-        GPIO.setup(4,GPIO.OUT)
-        GPIO.setup(17,GPIO.OUT)
-        GPIO.setup(27,GPIO.OUT)
 
-        col = player % 4
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(4,GPIO.OUT)
+    GPIO.setup(17,GPIO.OUT)
+    GPIO.setup(27,GPIO.OUT)
 
-        if col == 1:
-            GPIO.output(17,GPIO.LOW)
-            GPIO.output(27,GPIO.LOW)
-            GPIO.output(4,GPIO.HIGH)
+    col = player % 5
 
-        elif col == 2:
-            GPIO.output(4,GPIO.LOW)
-            GPIO.output(27,GPIO.LOW)
-            GPIO.output(17,GPIO.HIGH)
+    if col == 1:
+        GPIO.output(17,GPIO.LOW)
+        GPIO.output(27,GPIO.LOW)
+        GPIO.output(4,GPIO.HIGH)
 
-        elif col == 3:
-            GPIO.output(4,GPIO.LOW)
-            GPIO.output(17,GPIO.LOW)
-            GPIO.output(27,GPIO.HIGH)
+    elif col == 2:
+        GPIO.output(4,GPIO.LOW)
+        GPIO.output(27,GPIO.LOW)
+        GPIO.output(17,GPIO.HIGH)
 
-        else:
-            GPIO.output(4, GPIO.HIGH)
-            GPIO.output(17, GPIO.HIGH)
-            GPIO.output(27, GPIO.HIGH)
+    elif col == 3:
+        GPIO.output(4,GPIO.LOW)
+        GPIO.output(17,GPIO.LOW)
+        GPIO.output(27,GPIO.HIGH)
 
-    finally:
-        GPIO.cleanup()
+    elif col == 4:
+        GPIO.output(4, GPIO.LOW)
+        GPIO.output(17, GPIO.LOW)
+        GPIO.output(27, GPIO.LOW)
+
+    else:
+        GPIO.output(4, GPIO.HIGH)
+        GPIO.output(17, GPIO.HIGH)
+        GPIO.output(27, GPIO.HIGH)
+
 
 
 if __name__ == "__main__":
