2aa8be807b2fb0d2e161d8aead8bbe63b2630ecd
Michael Levy <mlevy94@vt.edu>
2017-04-08 17:06:11 -0400

    Integrated minecraft API into server code. Added basic automation.

--- Assignment4/mineclient.py
+++ Assignment4/mineclient.py
@@ -1,7 +1,27 @@
 import argparse
+from time import sleep
+
+blocks = [5, 57, 1]
+
+def getGameState():
+  return 0, 0, 0, 0
+
+def placeBlock(token, x, y, z, block):
+  pass
 
 
-
+def runGame(myToken):
+  top = myToken % 2 == 0
+  while True:
+    token, x, y, z = getGameState()
+    if token == 0:
+      break
+    elif token == myToken:
+      if top:
+        placeBlock(myToken, x, y + 1, z, blocks[myToken])
+      else:
+        placeBlock(myToken, x + 1, y, z, blocks[myToken])
+    sleep(1)
 
 
 
@@ -10,3 +30,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("server", nargs='?', default="localhost")
     fields = parser.parse_args()
+    
+    # get initial token
+    myToken = 1
+    runGame(myToken)
+
--- Assignment4/mineserver.py
+++ Assignment4/mineserver.py
@@ -1,12 +1,24 @@
+from mcpi.minecraft import Minecraft
 
 
+class MinePlayer:
+  def __init__(self):
+    self.mc = Minecraft.create()
+    self.mc.postToChat("I'm Alive!!!")
+  
+  def playerPosition(self):
+    # Find block player is standing on returns (x,y,z) coordinates
+    playerPos = self.mc.player.getTilePos()
+    return playerPos
+  
+  def setBlock(self, x, y, z, block):
+    # Set position and block
+    self.mc.player.setPos(x, y, z)
+    self.mc.setBlock(x + 1, y, z, block)
 
 
-
-
-
-
-
+def incrementToken():
+  pass
 
 
 
@@ -15,4 +27,12 @@
 
 
 if __name__ == "__main__":
-  pass
+  # set up broadcast server
+  # wait for 3 players to connect
+  print("Waiting for 3 players to connect...")
+  
+  input("All players have connected. Press any key to begin.")
+
+  # start accepting movement commands
+  
+  
