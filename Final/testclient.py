81446cbc4ff6a6e98e70b354e5db5eb289d425ec
jacques <jacques@vt.edu>
2017-04-28 13:52:57 -0400

    fixed

--- Final/testclient.py
+++ Final/testclient.py
@@ -3,7 +3,7 @@ from time import sleep
 
 host = input("host ip: ")
 client = SockClient(host)
-while 1
+while 1:
     outMessage = input("height to send: ")
     client.send(outMessage)
 
--- Final/testserver.py
+++ Final/testserver.py
@@ -2,6 +2,6 @@ from sockcomm import SockClient, SockServer
 from time import sleep
 server = SockServer('0.0.0.0')
 
-while 1
+while 1:
     sock, inMessage = server.recv()
     print("Server recieved height: ", inMessage)
