64a39a03d1f0de6a8e4117778b3f1bf53d841590
jacques <jacques@vt.edu>
2017-05-07 14:41:42 -0400

    unicode to str

--- Final/app.py
+++ Final/app.py
@@ -53,9 +53,9 @@ def update_task(task_id):
         abort(404)
     if not request.json:
         abort(400)
-    if 'title' in request.json and type(request.json['title']) != unicode:
+    if 'title' in request.json and type(request.json['title']) != str:
         abort(400)
-    if 'description' in request.json and type(request.json['description']) is not unicode:
+    if 'description' in request.json and type(request.json['description']) is not str:
         abort(400)
     if 'done' in request.json and type(request.json['done']) is not bool:
         abort(400)
