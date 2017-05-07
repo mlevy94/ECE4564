f79517251787e8844f9cb9bfad6b742fe36a5f33
jacques <jacques@vt.edu>
2017-05-07 11:39:50 -0400

    added memory

--- Final/app.py
+++ Final/app.py
@@ -3,9 +3,28 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+#@app.route('/')
+#def index():
+ #   return "Hello, World!"
+	
+@app.route('~/Documents/ECE4564/ECE4564/Final', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
 if __name__ == '__main__':
     app.run(debug=True)
