efd812856250a4c55438419102daca3ecf9fec63
jacques <jacques@vt.edu>
2017-05-09 16:58:14 -0400

    removed testclient and testserver

--- Final/testclient.py
+++
@@ -1,9 +0,0 @@
-from sockcomm import SockClient, SockServer
-from time import sleep
-
-host = input("host ip: ")
-client = SockClient(host)
-while 1:
-    outMessage = input("height to send: ")
-    client.send(outMessage)
-
--- Final/testserver.py
+++
@@ -1,7 +0,0 @@
-from sockcomm import SockClient, SockServer
-from time import sleep
-server = SockServer('0.0.0.0')
-
-while 1:
-    sock, inMessage = server.recv()
-    print("Server recieved height: ", inMessage)
